3eb0cb9a0cadf2c57c4e47b5b589a937d57d9e11
птичка <Sapunova270301@gmail.com>
2023-04-13 18:08:05 +0300

    change src/test/java/MaxServiceTest.java

--- src/test/java/MaxServiceTest.java
+++ src/test/java/MaxServiceTest.java
@@ -1,4 +1,31 @@
-import static org.junit.jupiter.api.Assertions.*;
-class MaxServiceTest {
-  
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MaxServiceTest {
+
+    @Test
+    public void shouldFindMax() {
+        MaxService service = new MaxService();
+
+        int a = 5;
+        int b = 3;
+
+        int expected = a;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindMaxIfB() {
+        MaxService service = new MaxService();
+
+        int a = 3;
+        int b = 5;
+
+        int expected = b;
+        int actual = service.max(a, b);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
